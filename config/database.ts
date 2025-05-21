1c7b90c6349ae6c9a1073971bab32a66b785f08d
houcine <houcine.taki@deadline.ma>
2025-05-22 00:48:51 +0100

    try fix issue

--- config/database.ts
+++ config/database.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: env("DATABASE_CLIENT", "postgres"),
     connection: {
       host: env("DATABASE_HOST", "localhost"),
       port: env.int("DATABASE_PORT", 5432),
