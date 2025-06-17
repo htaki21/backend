1f660a06d984e15db30c792b49e9ef8f827c3e22
houcine <houcine.taki@deadline.ma>
2025-06-17 12:06:37 +0100

    Fix: Revert custom router to fix production error

--- src/api/product/routes/product.ts
+++ src/api/product/routes/product.ts
@@ -4,24 +4,4 @@
 
 import { factories } from "@strapi/strapi";
 
-const defaultRouter = factories.createCoreRouter("api::product.product");
-
-// Create custom routes
-const customRoutes = [
-  {
-    method: "GET",
-    path: "/products",
-    handler: "api::product.product.find",
-    config: {
-      middlewares: [],
-      policies: [],
-    },
-  },
-];
-
-export default {
-  ...defaultRouter,
-  routes: Array.isArray(defaultRouter.routes)
-    ? [...defaultRouter.routes, ...customRoutes]
-    : [...defaultRouter.routes(), ...customRoutes],
-};
+export default factories.createCoreRouter("api::product.product");
